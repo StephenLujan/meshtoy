8fcb36566e14fe3387a6010cf3772bdc51c5903c
unbinder <bladedpenguin@gmail.com>
2015-10-02 14:06:55 -0400

    Updated main2 in meshtoy.py

--- meshtoy.py
+++ meshtoy.py
@@ -23,8 +23,8 @@ def LineSegmentFactory((x1, y1, z1), (x2, y2, z2), radius=1):
     return lambda x, y, z: x
 
 
-FREQUENCY = 0.3
-AMPLITUDE = 10.0
+FREQUENCY = 1.0
+AMPLITUDE = 100.0
 
 
 # Create the volume
@@ -36,8 +36,8 @@ def f(x, y, z):
 def main():
     logging.info("Example 2: Isosurface in Python function...")
     logging.info("(this might take a while...)")
-    samples = 100
-    bounds = math.sqrt(samples) + 1.0
+    samples = 200
+    bounds = 8
     t = time.time()
     # Extract the 16-isosurface
     vertices, triangles = mcubes.marching_cubes_func(
@@ -59,29 +59,37 @@ def main():
 
 def main2():
     t = time.time()
-    # from stl import mesh
+    #from stl import mesh
     diameter = 10
     radius = diameter / 2
-    half = radius / 2
+    half= radius / 2
 
-    X, Y, Z = np.mgrid[:diameter, :diameter, :diameter]
-    print "X"
-    print X
-    print "Y"
-    print Y
-    print "Z"
-    print Z
-    # u = (X-50)**2 + (Y-50)**2 + (Z-50)**2 - 25**2 + pnoise3(X,Y,Z,octaves=3)*3
-    u = (X - radius) ** 2 + (Y - radius) ** 2 + (Z - radius) ** 2 - half ** 2  # + pnoise3(X,Y,Z,octaves=3)*3
-    print "u"
-    print u
+    #X, Y, Z = np.mgrid[:diameter, :diameter, :diameter]
+    #print "X"
+    #print X
+    #print "Y"
+    #print Y
+    #print "Z"
+    #print Z
+    #u = (X-50)**2 + (Y-50)**2 + (Z-50)**2 - 25**2 + pnoise3(X,Y,Z,octaves=3)*3
+    #u = (X-radius)**2 + (Y-radius)**2 + (Z-radius)**2 - half**2 #+ pnoise3(X,Y,Z,octaves=3)*3
+    #u = [[[(X)**2 + (Y)**2 + (Z)**2 - 25**2 + pnoise3(X,Y,Z,octaves=3)*3 for X in range(-100,100)] for Y in range(-100,100)] for Z in range(-100,100)]
+    u = np.ndarray((200,200,200))
+    for X in range(-100,100):
+        for Y in range(-100,100):
+            for Z in range(-100,100):
+                
+                #print v
+                u[X+100,Y+100,Z+100] = ((X)**2 + (Y)**2 + (Z)**2) + pnoise3(X*0.05,Y*0.05,Z*0.05,octaves=3)*30
+    #print u
     # Extract the 0-isosurface
-    vertices, triangles = mcubes.marching_cubes(u, 0)
+    
+    print "u"
+    vertices, triangles = mcubes.marching_cubes(u, 60)
     print "mesh completed in %f seconds" % (time.time() - t)
 
-    # meshexport.export(vertices, triangles)
-    # meshexport.preview(triangles, vertices)
-
+    meshexport.export(vertices, triangles)
+    #meshexport.preview(triangles, vertices)
 
 if __name__ == "__main__":
     main()
